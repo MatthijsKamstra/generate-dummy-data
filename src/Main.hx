7b3381e74dfadeac6b087297a1a18eedc98ee8fd
Matthijs Kamstra <matthijs@fonk.amsterdam>
2020-12-19 20:24:06 +0100

    added year, body, dimensions, description

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,7 @@
 import haxe.Json;
 
+using StringTools;
+
 /**
 	{
 		created:
@@ -31,10 +33,17 @@ class Main {
 			Reflect.setField(obj, 'artist', getArtist());
 			Reflect.setField(obj, 'title', getTitle());
 			Reflect.setField(obj, 'type', getType());
-			Reflect.setField(obj, 'period', getPeriod());
 			Reflect.setField(obj, 'theme', getTheme());
 			Reflect.setField(obj, 'location', getLocation());
 			Reflect.setField(obj, 'picture', getPicture(i));
+			Reflect.setField(obj, 'dimensions', getDimensions());
+			Reflect.setField(obj, 'body', getBody());
+			Reflect.setField(obj, 'description', getDescription());
+
+			var _period = getPeriod();
+			Reflect.setField(obj, 'period', _period);
+			Reflect.setField(obj, 'year', getYear(_period));
+
 			arr.push(obj);
 		}
 		Reflect.setField(json, 'data', arr);
@@ -140,6 +149,10 @@ class Main {
 		};
 	}
 
+	function getYear(period) {
+		return randomInteger(period.start, period.end);
+	}
+
 	function getTheme() {
 		var arr = ['Innovation', 'Nature', 'Climate'];
 		return arr[Math.floor(Math.random() * arr.length)];
@@ -160,7 +173,29 @@ class Main {
 		return obj;
 	}
 
+	function getDimensions() {
+		var w = randomInteger(10, 2000);
+		var h = randomInteger(10, 2000);
+		return '${w} x ${h} cm';
+	}
+
+	var text = "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. \"What's happened to me?\" he thought. It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls. A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.";
+
+	function getBody() {
+		var l = text.length;
+		return capitalizeFirstLetter(text.substr(randomInteger(Math.round(l * 0.5), l)).trim());
+	}
+
+	function getDescription() {
+		var l = text.length;
+		return capitalizeFirstLetter(text.substr(randomInteger(Math.round(l * 0.5), l)).trim());
+	}
+
 	// ____________________________________ misc ____________________________________
+
+	function capitalizeFirstLetter(string:String) {
+		return string.charAt(0).toUpperCase() + string.substr(1);
+	}
 
 	function randomInteger(min:Int, ?max:Int):Int {
 		if (max == null) {
