b9e507646a024969d14487a72f8ec6eacb458db2
Matthijs Kamstra <matthijs@fonk.amsterdam>
2022-06-13 10:27:50 +0200

    remove space

--- src/name/LastName.hx
+++ src/name/LastName.hx
@@ -3,7 +3,7 @@ package name;
 class LastName {
 	public static function get():String {
 		var arr:Array<String> = [
-			"De Jong", "Jansen", "De Vries", "Van den Berg ", "Van Dijk", "Bakker", "Janssen", "Visser", "Smit", "Meijer", "De Boer", "Mulder", "De Groot",
+			"De Jong", "Jansen", "De Vries", "Van den Berg", "Van Dijk", "Bakker", "Janssen", "Visser", "Smit", "Meijer", "De Boer", "Mulder", "De Groot",
 			"Bos", "Vos", "Peters", "Hendriks", "Van Leeuwen", "Dekker", "Brouwer", "De Wit", "Dijkstra", "Smits", "De Graaf", "Van der Meer"
 		];
 
