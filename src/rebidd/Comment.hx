da57fb8ad64f3a5d03545bad6385652ff145170f
Matthijs Kamstra <matthijs@fonk.amsterdam>
2022-06-13 15:23:44 +0200

    more space between creation and update

--- src/rebidd/Comment.hx
+++ src/rebidd/Comment.hx
@@ -16,8 +16,8 @@ class Comment {
 		return {
 			id: utils.UUID.uuid(),
 			text: new Lorem().title(),
-			created_date: toISOString(Date.now()),
-			updated_date: toISOString(Date.now()),
+			created_date: toISOString(new Dates().past(MathUtil.randomInteger(10, 20))),
+			updated_date: toISOString(new Dates().past(MathUtil.randomInteger(0, 10))),
 			"User": new Rebidd().user()
 		};
 	}
